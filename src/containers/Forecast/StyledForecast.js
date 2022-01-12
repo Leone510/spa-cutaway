3e8d6f16c7ff0fce85c4d4ee19d34e552b9b9fa3
Leone510 <meteorologsio@gmail.com>
2022-01-12 20:03:48 +0200

    webkit fill available

--- src/containers/Forecast/StyledForecast.js
+++ src/containers/Forecast/StyledForecast.js
@@ -4,6 +4,7 @@ import { device } from '../../data/breakpoints';
 export const StyledForecust = styled.div`
    width: 100%;
    height: calc(100vh - 100px);
+   min-height: -webkit-fill-available;
 
    @media ${device.tablet} {
       height: calc(100vh - 150px);
