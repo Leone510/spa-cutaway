b09ccae60cbff0570534627daec8dd3e5a8caf3e
Leone510 <meteorologsio@gmail.com>
2022-01-21 19:55:34 +0200

    fix button shadow

--- src/containers/Cutaway/DemoCard/DemoCard.jsx
+++ src/containers/Cutaway/DemoCard/DemoCard.jsx
@@ -1,5 +1,6 @@
 import { useHistory } from "react-router";
 import { CutawayBtn } from "../CutawayBtn/CutawayBtn";
+import { StyledDemoBtnWrapper } from "./StyledDemoBtnWrapper";
 import { StyledDemoCard } from "./StyledDemoCard";
 
 export const DemoCard = ({img, page}) => {
@@ -7,10 +8,16 @@ export const DemoCard = ({img, page}) => {
 
    return (
      <StyledDemoCard img={img}>
-         <CutawayBtn onClick={() => history.push(page)
-         }>
-            <p>Go to work...</p>
-         </CutawayBtn>
+
+         <StyledDemoBtnWrapper>
+
+            <CutawayBtn onClick={() => history.push(page)
+            }>
+               <p>Go to work...</p>
+            </CutawayBtn>
+            
+         </StyledDemoBtnWrapper>
+
      </StyledDemoCard>
    )
 }
---
+++ src/containers/Cutaway/DemoCard/StyledDemoBtnWrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const StyledDemoBtnWrapper = styled.div`
+   width: 400px;
+   height: 150px;
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   background: radial-gradient(rgba(255,255,255,1) 20%, rgba(255,255,255,0) 70%);
+   background-size: auto;
+`
